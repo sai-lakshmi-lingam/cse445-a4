b13996dbb7b9bd59e9e96147080573d80a378155
sai :) <smlingam@asu.edu>
2025-04-04 21:47:20 -0700

    moved things back

---
+++ submission.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
+namespace ConsoleApp1
+{
+
+
+    public class Program
+    {
+        public static string xmlURL = "https://raw.githubusercontent.com/sai-lakshmi-lingam/cse445-a4/main/Hotels.xml";
+        public static string xmlErrorURL = "https://raw.githubusercontent.com/sai-lakshmi-lingam/cse445-a4/main/HotelsErrors.xml";
+        public static string xsdURL = "https://raw.githubusercontent.com/sai-lakshmi-lingam/cse445-a4/main/Hotels.xsd";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Ignore;
+                settings.Schemas.Add(null, xsdUrl);
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationEventHandler += (sender, e) => {
+                    throw new XmlSchemaValidationException(e.Message);
+                };
+
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+                {
+                    while (reader.Read()) { }
+                }
+
+                return "No Error";
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+            
+
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            //return jsonText;
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlUrl);
+
+                string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+                return jsonText;
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+
+        }
+    }
+
+}
